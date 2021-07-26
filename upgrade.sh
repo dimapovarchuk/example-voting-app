758b0e53bb7bcd3180aba229069be9fb495e2d8e
burynk <40724806+burynk@users.noreply.github.com>
2021-07-26 23:07:29 +0300

    🤠

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-    docker-compose -f /home/ubuntu/example-voting-app/docker-compose.yml up --build -d
+docker system prune -f
+docker-compose -f /home/ubuntu/example-voting-app/docker-compose.yml up --build -d
