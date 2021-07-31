c4629217df34e22b563c96c8b2723ea811e802a3
burynk <40724806+burynk@users.noreply.github.com>
2021-07-31 15:02:40 +0300

    🍆

--- vote/app.py
+++ vote/app.py
@@ -19,7 +19,7 @@ app.logger.setLevel(logging.INFO)
 redis_host = os.environ['REDIS_HOST']
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis_host", db=0, socket_timeout=5)
+        g.redis = Redis(host=redis_host, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
