eb39f8832590ea11526ae28c77c63be66ab25243
burynk <40724806+burynk@users.noreply.github.com>
2021-08-01 02:12:24 +0300

    UGH

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
 docker system prune -f
-cd/home/ubuntu/example-voting-app/ 
+cd /home/ubuntu/example-voting-app/ 
 docker-compose pull
 docker-compose -f /home/ubuntu/example-voting-app/docker-compose.yml up --build -d
