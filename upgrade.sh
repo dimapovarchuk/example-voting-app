fc086a3b7fed359709a3dc9a651aef4df66bec64
burynk <40724806+burynk@users.noreply.github.com>
2021-08-01 22:24:02 +0300

    followed roman's advice, let's see

--- upgrade.sh
+++ upgrade.sh
@@ -3,4 +3,5 @@
 docker system prune -f
 cd /home/ubuntu/example-voting-app/ 
 git pull
-docker-compose -f /home/ubuntu/example-voting-app/docker-compose.yml up --build -d
+docker-compose pull
+docker-compose up -d
